3ef88ed8592082aa2e37f5a758ff7c8b69c56012
amamolo9986 <amamolo9986@yahoo.com>
2024-05-16 19:55:05 -0500

    Initial commit

---
+++ src/main/java/com/coderscampus/javascriptpractice/Week21RefresherApplication.java
@@ -0,0 +1,13 @@
+package com.coderscampus.javascriptpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week21RefresherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week21RefresherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coderscampus/javascriptpractice/Week21RefresherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.javascriptpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week21RefresherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
