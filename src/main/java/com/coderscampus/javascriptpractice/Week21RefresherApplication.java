459ff10debed10a8cf8fc8183f43a0f3fb383f98
amamolo9986 <amamolo9986@yahoo.com>
2024-05-20 18:50:28 -0500

    notes

--- src/main/java/com/coderscampus/javascriptpractice/Week21RefresherApplication.java
+++ src/main/java/com/coderscampus/javascriptpractice/Week21RefresherApplication.java
@@ -12,14 +12,19 @@ public class Week21RefresherApplication {
 
 	/* JS Notes
 	 * 
-	 * Video 1: Just setting up the project
+	 * Video 1: Setting up the project
 	 * 
-	 * Video 2: JS lives in a web page and a browser is in charge of working with JS.
+	 * Video 2: Hello World
+	 * JS lives in a web page and a browser is in charge of working with JS.
 	 * They essentially have JS engines that can be used to interpret.
 	 * 
 	 * "Client Side" language. JS can be ran on the server, but thats Node JS. 
 	 * 
-	 * Is declared in the head or the body of HTML page
+	 * Is declared in the head or the body of HTML page using <script><script/>
+	 * 
+	 * Video 3: Declaring Variables in JS
+	 * Data types exist but they are handled for us by the JS engine. We dont 
+	 * define types explicitly. we say "var variableName = literal"
 	 * 
 	 */
 	
